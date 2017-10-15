51df1fc64dea52d76194814e660293ef877ae9e8
rheng001 <rheng001@gmail.com>
2017-10-15 02:01:09 -0700

    Updated

--- js/headerScript.js
+++ js/headerScript.js
@@ -1,11 +1,9 @@
 document.write(
 "<aside class='topNav nav-blue' id='topNav'>" +
 		"<div class='container navBar'>" +
-			"<a href='http://michaelreeves.us/'><h2>Home</h2> </a>" +
-			"<a href='mailto:michaelreeves808@gmail.com?Subject=Hi%20Michael'><h2>E-Mail</h2> </a>" +
-			"<a href='http://michaelreeves.us/resume.pdf'><h2>Resume</h2> </a>" +
-			"<a href='https://www.youtube.com/channel/UCtHaxi4GTYDpJgMSGy7AeSw'><h2>YouTube</h2> </a>"+
-			"<a href='https://michaelreeves.us/pages/tutoring.html' target='_blank'><h2>Tutoring</h2></a>" +
+			"<a href='http://www.richardheng.me/'><h2>Home</h2> </a>" +
+			"<a href='http://www.richardheng.me/'><h2>Resume</h2> </a>" +
+			"<a href='mailto:rheng009@gmail.com?Subject=Hi%20Richard'><h2>E-Mail</h2> </a>" +
 		"</div>" +
 	"</aside>"
 );
