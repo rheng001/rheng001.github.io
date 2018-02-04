0c58e166a96a6f65207c8a9512e8b103ee2770e2
Richard Heng <rheng001@gmail.com>
2018-02-04 15:49:00 -0800

    Updated

--- js/headerScript.js
+++
@@ -1,9 +0,0 @@
-document.write(
-"<aside class='topNav nav-blue' id='topNav'>" +
-		"<div class='container navBar'>" +
-			"<a href='http://www.richardheng.me/'><h2>Home</h2> </a>" +
-			"<a href='http://www.richardheng.me/'><h2>Resume</h2> </a>" +
-			"<a href='mailto:rheng009@gmail.com?Subject=Hi%20Richard'><h2>E-Mail</h2> </a>" +
-		"</div>" +
-	"</aside>"
-);
